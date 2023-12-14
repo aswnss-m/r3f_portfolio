19f87cca49f87e604f6e995c4ffcf1f9d21020c9
Aswin Lal M <aswnss.m@gmail.com>
2023-12-14 15:10:52 +0530

    hero section layout

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -3,11 +3,11 @@ import {Link} from "react-router-dom"
 function Navbar() {
   return (
     <div className='flex justify-between px-10 py-5 items-end z-[100] absolute w-full top-0'>
-      <h1 className='font-bold text-zircon-50 text-xl'>Aswnss</h1>
+      <h1 className='font-bold text-xl'>Aswnss</h1>
       <ul className='flex gap-2'>
-        <li className='text-zircon-50 hover:underline'><Link to={'/'}>About</Link></li>
-        <li className='text-zircon-50 hover:underline'><Link to={'/'}>Projects</Link></li>
-        <li className='text-zircon-50 hover:underline'><Link to={'/'}>Contact</Link></li>
+        <li className=' hover:underline'><Link to={'/'}>About</Link></li>
+        <li className=' hover:underline'><Link to={'/'}>Projects</Link></li>
+        <li className=' hover:underline'><Link to={'/'}>Contact</Link></li>
       </ul>
     </div>
   )
--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 function Hero() {
     return (
-        <div className="w-full flex justify-start h-[100%]">
-            <div className=" aspect-square flex justify-end flex-col p-10">
-                <h1 className='text-[10vw] md:text-[5vw] font-semibold leading-3'>3D Artist <span className='font-light'>&</span></h1>
+        <div className={` w-full h-full p-10 grid 
+                            grid-cols-1 grid-rows-1
+                            md:grid-cols-2 md:grid-rows-2`}>
+
+            <div className="md:row-start-2 flex flex-col justify-end">
+                <h1 className='text-[10vw] md:text-[5vw] font-semibold'>3D Artist <span className='font-light'>&</span></h1>
                 <h1 className='text-[10vw] md:text-[5vw] font-semibold'>Web Developer</h1>
+            </div>
+            <div className={`flex md:justify-end items-end row-start-2 md:col-start-2`}>
+            <button className={`font-medium underline underline-offset-2 tracking-widest`}>Get in touch</button>
             </div>
         </div>
     )
